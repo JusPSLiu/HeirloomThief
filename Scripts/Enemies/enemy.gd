1adf04328017c23f62e36e638c64e52733abe366
JusPSLiu <juspsliu@yahoo.com>
2024-09-13 17:22:40 -0400

    slight adjustments to health and shaders

--- Scripts/Enemies/enemy.gd
+++ Scripts/Enemies/enemy.gd
@@ -5,26 +5,19 @@ class_name Enemy extends CharacterBody2D
 @export var max_speed : float
 @export var gravity : float
 
-@onready var soundPlayerNode : AudioStreamPlayer2D = get_node("sounds")
 @onready var startPos : Vector2 = position
+@onready var healthBar : ColorRect = get_node("HealthBar/health")
+@onready var soundGetHit : AudioStreamPlayer2D = get_node("hit")
+@onready var soundDeath : AudioStreamPlayer2D = get_node("hit/die")
 
 # functional variables
 var current_health : int
 var current_speed : int
-var soundEffects : AudioStreamPlaybackPolyphonic
 
-const hit_sound = preload("res://Sounds/player/hitty.wav")
-const death_sound = preload("res://Sounds/enemy/boom.wav")
 const deathparticle = preload("res://Scenes/Enemies/enemy_death_effect.tscn")
 
 func _ready() -> void:
 	current_health = max_health
-	## DEBUG
-	# TODO: DELETE THIS. ITS JUST FOR TESTING PURPOSES
-	# ITS PROBABLY FINE IF WE KEEP IT THO
-	soundPlayerNode.stream = AudioStreamPolyphonic.new()
-	soundPlayerNode.play()
-	soundEffects = soundPlayerNode.get_stream_playback()
 	
 	# OKAY ACTUALLY DO DELETE THIS WHEN LEVELS ARE IMPLEMENTED.
 	# ITS JUST TO TEST RESPAWNING
@@ -43,32 +36,31 @@ func _physics_process(delta: float) -> void:
 
 func _on_visible_on_screen_enabler_2d_screen_entered() -> void:
 	set_physics_process(true)
-	soundPlayerNode.stream = AudioStreamPolyphonic.new()
-	soundPlayerNode.play()
-	soundEffects = soundPlayerNode.get_stream_playback()
 	respawn()
 
 
 func _on_visible_on_screen_enabler_2d_screen_exited() -> void:
 	set_physics_process(false)
-	soundEffects.is_stream_playing(0)
 	#soundPlayerNode.stop()
 	
 
 func damage(damage_val):
 	current_health -= damage_val
-	if (current_health < 0):
+	healthBar.size.x = max(0, (10*current_health/max_health))
+	if (current_health <= 0):
 		hide()
 		$Timer.start()
-		if (soundEffects):
-			soundEffects.play_stream(death_sound)
+		if (soundDeath):
+			soundDeath.play()
 		var deathy = deathparticle.instantiate()
 		deathy.position = position + Vector2(0, -16)
 		add_sibling(deathy)
-	elif (soundEffects):
-		soundEffects.play_stream(hit_sound)
+	elif (soundGetHit):
+		soundGetHit.play()
 
 func respawn():
 	position = startPos
+	current_health = max_health
+	healthBar.size.x = 10
 	velocity = Vector2.ZERO
 	show()
--- Scripts/Player/attackbox.gd
+++ Scripts/Player/attackbox.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@export var damage_value : int = 20
+@export var damage_value : int = 1
 
 func _on_body_entered(body: Node2D) -> void:
 	if (body.is_in_group("enemy")):
