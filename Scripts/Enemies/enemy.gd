f5a308b777592c9a7765c90ab5bb68896690eb5c
JusPSLiu <juspsliu@yahoo.com>
2024-09-15 03:25:22 -0400

    fixed enemy spawning issues

--- Scripts/Enemies/enemy.gd
+++ Scripts/Enemies/enemy.gd
@@ -4,6 +4,7 @@ class_name Enemy extends CharacterBody2D
 @export var max_health : int
 @export var max_speed : float
 @export var gravity : float
+@export var damage_component : Area2D
 
 @onready var startPos : Vector2 = position
 @onready var healthBar : ColorRect = get_node("HealthBar/health")
@@ -13,16 +14,12 @@ class_name Enemy extends CharacterBody2D
 # functional variables
 var current_health : int
 var current_speed : int
+var home_area : int = -1
 
 const deathparticle = preload("res://Scenes/Enemies/enemy_death_effect.tscn")
 
 func _ready() -> void:
 	current_health = max_health
-	
-	# OKAY ACTUALLY DO DELETE THIS WHEN LEVELS ARE IMPLEMENTED.
-	# ITS JUST TO TEST RESPAWNING
-	$Timer.wait_time = 15.0  # Set the timer to 20 seconds
-	$Timer.connect("timeout", respawn)
 
 
 func _physics_process(delta: float) -> void:
@@ -33,28 +30,31 @@ func _physics_process(delta: float) -> void:
 	# Collision
 	move_and_slide()
 
-
-func _on_visible_on_screen_enabler_2d_screen_entered() -> void:
+func enable():
 	set_physics_process(true)
+	set_collision_layer_value(1, true)
+	if (damage_component):
+		damage_component.process_mode = Node.PROCESS_MODE_INHERIT
 	respawn()
 
 
-func _on_visible_on_screen_enabler_2d_screen_exited() -> void:
+func disable():
+	hide()
 	set_physics_process(false)
-	#soundPlayerNode.stop()
-	
+	set_collision_layer_value(1, false)
+	if (damage_component):
+		damage_component.process_mode = Node.PROCESS_MODE_DISABLED
 
 func damage(damage_val):
 	current_health -= damage_val
 	healthBar.size.x = max(0, (10*current_health/max_health))
 	if (current_health <= 0):
-		hide()
-		$Timer.start()
 		if (soundDeath):
 			soundDeath.play()
 		var deathy = deathparticle.instantiate()
 		deathy.position = position + Vector2(0, -16)
 		add_sibling(deathy)
+		disable()
 	elif (soundGetHit):
 		soundGetHit.play()
 
--- Scripts/room_script.gd
+++ Scripts/room_script.gd
@@ -1,3 +1,19 @@
 extends Area2D
 
 @export var room_number : int = 0
+
+var my_enemies : Array[CharacterBody2D]
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if (body.is_in_group("enemy") and body not in my_enemies):
+		if (body.home_area < 0):
+			body.home_area = room_number
+			my_enemies.append(body)
+			if (SaveManager.current_room != room_number):
+				body.disable()
+		elif (body.home_area != room_number):
+			body.disable()
+	elif (body.is_in_group("Player")):
+		for enemy in my_enemies:
+			enemy.enable()
